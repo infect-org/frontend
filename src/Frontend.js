2d2617c240e8d4a950d3381b29ff19f68acb0c19
Michael van der Weg <michael@joinbox.com>
2016-11-12 22:07:44 +0100

    boilerplate

---
+++ src/Frontend.js
@@ -0,0 +1,20 @@
+(function() {
+	'use strict';
+
+
+	const distributed = require('distributed-prototype');
+
+
+
+
+
+
+	module.exports = class InfectFrontend extends distributed.Website {
+
+
+
+		constructor(options) {
+			super(options);
+		}
+	}
+})();
