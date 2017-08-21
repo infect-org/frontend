503d1f82f22b176ac789e144408be1bf4ed031c2
Sandro Kneubühl <sandro.kneubuehl@gmail.com>
2017-08-21 16:53:05 +0200

    new GUI (alpha)
    - removed less
    - add scss
    - webpack config (added scss support)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
 const BeepPlugin = require('webpack-beep-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = [
 
 	// Main app
 	{
-		context: path.resolve(__dirname, 'www/src/js')
-		, entry:  [
-			'./main.js'
-		]
+		context: path.resolve(__dirname, 'www')
+		, entry:  {
+			home: ['./src/js/main.js']
+			,styles: ['./styles/scss/main.scss']
+		}
 		, output: {
-			path: path.resolve(__dirname, 'www/dist/js/')
-			, publicPath: '/dist/js/'
-			, filename: 'main.js'
+			path: path.resolve(__dirname, 'www/dist/')
+			, publicPath: '/dist/js'
+			, filename: 'js/[name].js'
 		}
 		, devtool: 'source-map'
 		, devServer: {
-			publicPath: '/dist/js/'
+			publicPath: '/dist/'
 			, contentBase: 'www'
 			, hot: true
 		}
@@ -31,12 +33,20 @@ module.exports = [
 						, path.resolve(__dirname, 'node_modules/mobx')
 					]
 					/* query is taken from .babelrc */
+				}, 
+				{
+			  		test: /\.scss$/,
+			  		loader: ExtractTextPlugin.extract('css-loader!sass-loader')
 				}
 			]
 		}
 		, resolve: {
 			extensions: ['.js', '.jsx']
 		}
-		, plugins: [new BeepPlugin()]
+		, plugins: [new ExtractTextPlugin(
+						{filename: '/css/main.min.css',
+      					allChunks: false
+    				})
+					, new BeepPlugin()]
 	}
 ];
