a3fde14e2e9739d4d6dbdfb7440d0824e4eec981
Felix Steiner <felix_steiner@bluewin.ch>
2018-08-10 16:30:41 +0200

    feat(*): add cache busting

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const rev = require('gulp-rev');
+const revCollect = require('gulp-rev-collector');
+const revDelete = require('rev-del');
 const BuildTask = require('@joinbox/build-task');
 
 const builder = new BuildTask();
@@ -36,8 +39,36 @@ builder.tasks.set('imgWatch', imgWatch);
 
 builder.devTasks.push(imgFiles);
 builder.watchTasks.push(imgWatch);
+builder.prodTasks.push(imgFiles);
 
 
+// Cache
+function bustRename() {
+    const distFolder = 'www/dist';
+    return gulp.src([`${distFolder}/**/*.js`, `${distFolder}/**/*.css`])
+        .pipe(rev())
+        .pipe(gulp.dest(distFolder))
+        .pipe(rev.manifest())
+        .pipe(revDelete({ dest: distFolder }))
+        .pipe(revDelete())
+        .pipe(gulp.dest(distFolder));
+} 
+function bustHtml() {
+    const distFolder = 'www/dist';
+    return gulp.src([`${distFolder}/rev-manifest.json`, `${distFolder}/**/*.{html,css,js}`])
+        .pipe(revCollect())
+        .pipe(gulp.dest(distFolder));
+}
 
+builder.tasks.set('bustRename', bustRename);
+builder.tasks.set('bustHtml', bustHtml);
 
-module.exports = builder.createTasks();
+// Now export all tasks
+const tasks = builder.createTasks();
+
+// Create a new main build task as all build tasks in BuildTask are executed in parallel (and
+// we need to wait for all tasks to complete before we can bust).
+tasks.infectProd = gulp.series(builder.tasks.get('prod'), bustRename, bustHtml);
+
+module.exports = tasks;
+
